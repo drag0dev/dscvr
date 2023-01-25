af193b2520c4c398607be994f356f66dd3aa2c57
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-01-25 18:47:19 +0100

    missing source for ip

--- ip.c
+++ ip.c
@@ -1,1 +1,18 @@
 #include "ip.h"
+
+void iptoo(IPv4* ip, char* ipString, int len) {
+    int i = 0;
+    int j = 0;
+    while (i < 4 && j < len) {
+        ip->octets[i] = 0;
+        while (*ipString != '.' && j < len) {
+            ip->octets[i] *= 10;
+            ip->octets[i] += (*ipString) - 48;
+            ipString++;
+            j++;
+        }
+        ipString++;
+        j++;
+        i++;
+    }
+}
