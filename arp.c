868c70b82cc340c0c3d6df8f3800f8f381473f67
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-01-28 19:42:37 +0100

    finished sending arp, basic reading of response

--- arp.c
+++ arp.c
@@ -1,4 +1,5 @@
 #include "arp.h"
+#include "get.h"
 #include <netinet/in.h>
 #include <string.h>
 #include <stdio.h>
@@ -31,10 +32,11 @@ int bind_arp(int* fd, int ifidx) {
     return 0;
 }
 
-int send_arp(int fd, char* ip, int ifidx) {
+int send_arp(int fd, char* targetIp, interfaceInfo* ifInfo) {
     struct sockaddr_ll addr;
+    memset(&addr, 0, sizeof(struct sockaddr_ll));
     addr.sll_family = AF_PACKET;
-    addr.sll_ifindex = ifidx;
+    addr.sll_ifindex = ifInfo->ifidex;
     addr.sll_halen = ETHER_ADDR_LEN;
     addr.sll_protocol = htons(ETH_P_ARP);
 
@@ -48,17 +50,59 @@ int send_arp(int fd, char* ip, int ifidx) {
     req.arp_hln = ETHER_ADDR_LEN; // hw address len
     req.arp_pln = sizeof(in_addr_t); // protocol address len
     req.arp_op = htons(ARPOP_REQUEST); // arp opcode
-    memset(&req.arp_tha, 0, sizeof(req.arp_tha));
+    memset(&req.arp_tha, 0, sizeof(req.arp_tha)); // mac address 0 - broadcast
 
     struct in_addr targetIPAddr;
     memset(&targetIPAddr, 0, sizeof(struct in_addr));
-    inet_aton(ip, &targetIPAddr); // it never fails since passed ips are generated internally
+    inet_aton(targetIp, &targetIPAddr); // it never fails since passed ips are generated internally
     memcpy(&req.arp_tpa, &targetIPAddr.s_addr, sizeof(req.arp_tpa));
+
+    // source ip
+    memcpy(req.arp_spa, &ifInfo->ip->s_addr, sizeof(ifInfo->ip->s_addr));
+
+    // source mac
+    memcpy(&req.arp_sha, ifInfo->mac, sizeof(req.arp_sha));
 
     if (sendto(fd, &req, sizeof(req), 0, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
         printf("Error sending ARP requst\n");
         return -1;
     }
+
+    return 0;
+}
+
+int read_arp(int fd) {
+    // TODO: timeout when we cant find a device/poll?
+    // TODO: handle when received more than 1024? is that even possible?
+    unsigned char buffer[1024];
+    ssize_t length = recvfrom(fd, buffer, 1024, 0, NULL, NULL);
+    int index;
+    if (length == -1) {
+        perror("recvfrom()");
+        return -1;
+    }
+
+    struct arp_header *arp_resp = (struct arp_header *) buffer;
+
+    struct in_addr sender_a;
+    memset(&sender_a, 0, sizeof(struct in_addr));
+    memcpy(&sender_a.s_addr, arp_resp->sender_ip, sizeof(uint32_t));
+
+    printf("arp opcode: %x\n", htons(arp_resp->opcode));
+    printf("Sender MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
+          arp_resp->sender_mac[0],
+          arp_resp->sender_mac[1],
+          arp_resp->sender_mac[2],
+          arp_resp->sender_mac[3],
+          arp_resp->sender_mac[4],
+          arp_resp->sender_mac[5]);
+    printf("Target MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
+          arp_resp->target_mac[0],
+          arp_resp->target_mac[1],
+          arp_resp->target_mac[2],
+          arp_resp->target_mac[3],
+          arp_resp->target_mac[4],
+          arp_resp->target_mac[5]);
 
     return 0;
 }
--- arp.h
+++ arp.h
@@ -1,9 +1,26 @@
 #ifndef ARP_H_
 #define ARP_H_
 
+#include "get.h"
 #include <stdint.h>
 
+#define MAC_LENGTH 6
+#define IPV4_LENGTH 4
+
+struct arp_header {
+    unsigned short hardware_type;
+    unsigned short protocol_type;
+    unsigned char hardware_len;
+    unsigned char protocol_len;
+    unsigned short opcode;
+    unsigned char sender_mac[MAC_LENGTH];
+    unsigned char sender_ip[IPV4_LENGTH];
+    unsigned char target_mac[MAC_LENGTH];
+    unsigned char target_ip[IPV4_LENGTH];
+};
+
 int bind_arp(int* fd, int ifidx);
-int send_arp(int fd, char* ip, int ifidx);
+int send_arp(int fd, char* targetIp, interfaceInfo* ifInfo);
+int read_arp(int fd);
 
 #endif // ARP_H_
