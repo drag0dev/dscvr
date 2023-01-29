bfc419da8f07d9a1a28cbe0a15451af591f21bb2
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-01-29 17:51:39 +0100

    implement getting network address

--- masking.c
+++ masking.c
@@ -1,1 +1,32 @@
 #include "masking.h"
+#include "ip.h"
+#include <stdlib.h>
+#include <string.h>
+
+unsigned char createMask(int maskLen) {
+    unsigned char mask = 0;
+    for (int i = 0; i < 8; ++i) {
+        mask = mask << 1;
+        if (maskLen > 0) {
+            ++mask;
+            --maskLen;
+        }
+    }
+    return mask;
+}
+
+IPv4* getNetworkAddress(IPv4* src, unsigned int maskLen) {
+    IPv4* res = malloc(sizeof(IPv4));
+    if (res == NULL) return NULL;
+
+    unsigned short mask;
+    for (int i = 0; i < 4; ++i) {
+        if (maskLen > 0) {
+            mask = createMask(maskLen);
+            if (maskLen >= 8) maskLen -= 8;
+            else maskLen = 0;
+        } else mask = 0;
+        res->octets[i] = src->octets[i] & mask;
+    }
+    return res;
+}
--- masking.h
+++ masking.h
@@ -3,9 +3,10 @@
 
 #include "ip.h"
 
-/* src ip will be changed in place to network address based on the mask*/
-void getNetworkAddress(IPv4* src, unsigned int mask);
+/* src ip will be changed to network address based on the mask and returned*/
+IPv4* getNetworkAddress(IPv4* src, unsigned int mask);
 
+// TODO: do we really need this
 /* src ip will be changed in place to broadcast address based on the mask*/
 void getBroadcastAddress(IPv4* src, unsigned int mask);
 
