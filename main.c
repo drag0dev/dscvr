829e259903a9c4021f4b5fbb55809568ba3811b7
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-02-06 19:00:46 +0100

    final cleanup

--- main.c
+++ main.c
@@ -11,9 +11,6 @@
 #include "ip.h"
 #include "masking.h"
 
-// TODO: cant know which interface we want, list all ips and interfacesa and ask user which one to use
-// TODO: based on the interface can get default mask, and then confirm with user, if not user can enter
-
 // small mem leaks in case of early returns are left for sake of readability
 int main () {
     interfaceInfo* ifInfo = getInterface();
